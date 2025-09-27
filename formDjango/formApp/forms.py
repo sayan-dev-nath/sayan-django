82d031a214b2c1d1b42fb25fb1665f5a599495b1
sayan-dev-nath <sayandevnath01@gmail.com>
2025-09-27 16:12:31 +0600

    update

--- formDjango/formApp/forms.py
+++ formDjango/formApp/forms.py
@@ -4,3 +4,9 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(label="User Name")
     email = forms.EmailField(label="User Email")
+    age = forms.IntegerField(label="User Age")
+    weight = forms.FloatField(label="User Weight")
+    balance = forms.DecimalField(label="User Balance")
+    check = forms.BooleanField(label="User Check")
+    birthday = forms.DateField(label="Birthday")
+    appointment = forms.DateTimeField()
