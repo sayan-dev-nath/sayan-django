751d76f5822effb6af539b3997535d2084519c24
sayan-devn-nath <sayandevnath01@gmail.com>
2025-10-26 23:37:54 +0600

    update

--- Authentication/room/app1/urls.py
+++ Authentication/room/app1/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import home, signuppage, profile, loginpage, user_logout
+from .views import (
+    home,
+    signuppage,
+    profile,
+    loginpage,
+    user_logout,
+    password_change,
+    password_change1,
+)
 
 urlpatterns = [
     path("", home, name="home"),
@@ -7,4 +15,6 @@ urlpatterns = [
     path("profile/", profile, name="profile"),
     path("loginpage/", loginpage, name="loginpage"),
     path("user_logout/", user_logout, name="user_logout"),
+    path("password_change/", password_change, name="password_change"),
+    path("password_change1/", password_change1, name="password_change1"),
 ]
