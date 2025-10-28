5e88a93871d6f5de229b73e1ab6b68834d2b023b
sayan-devn-nath <sayandevnath01@gmail.com>
2025-10-28 18:51:36 +0600

    update

---
+++ MealProject/room1/meals/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Food
+
+
+class MealAddForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
+    class Meta:
+        model = Food
+        fields = "__all__"
--- room1/meals/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def show_meals(request):
-    return render(request, "meals/index.html")
