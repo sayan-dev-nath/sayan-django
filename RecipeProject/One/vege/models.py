c5e85c7a968dce6430bd63da28d04bf18dd0ff0d
sayan-dev-nath <sayandevnath01@gmail.com>
2025-11-01 00:03:21 +0600

    update

--- RecipeProject/One/vege/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class Recipe(models.Model):
-    recipe_name = models.CharField(max_length=255)
-    recipe_description = models.TextField()
-    recipe_image = models.ImageField(upload_to="recipe")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
